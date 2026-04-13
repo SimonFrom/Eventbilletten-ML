6f066b45d137a2305074e7eaadcac26ccff57535
Simon From <simon.bulder@gmail.com>
2026-04-13 13:05:31 +0200

    lkdsflkjdsfsaad

--- main.py
+++ main.py
@@ -26,16 +26,53 @@ def cleanOrderData():
 
     return ordersNonNull
 
-mapped_events = getEventsForSeller(test_key)
+def cleanEventData():
+    data = getEventsForSeller(test_key)
+    eventsDF = pd.DataFrame(data["events"])
+    print("Events count:")
+    print(len(eventsDF))
+    eventsNonGroup = eventsDF[eventsDF['event_type'] == 'GROUP']
 
+    return eventsNonGroup
+
+finalEvents = cleanEventData()
+# mapped_events = getEventsForSeller(test_key)
 finalOrders = cleanOrderData()
-print("Orders cleaned:")
-print(len(finalOrders))
-print(finalOrders["postal"].unique())
-print(finalOrders.describe().to_string())
+
+mergedDF = pd.merge(finalEvents, finalOrders, left_on='id', right_on='event_id')
+
+# Orders per event
+print("Orders per event")
+print(mergedDF.groupby('name_x')['id_y'].count())
+
+# Revenue per event
+print("Revenue per event")
+print(mergedDF.groupby('name_x')['real_price'].sum())
+
+# Unique customers per event
+print("Unique customers per event")
+print(mergedDF.groupby('name_x')['email'].nunique())
+
+# Orders per city
+print("Orders per city")
+print(mergedDF.groupby('city')['id_y'].count().sort_values(ascending=False))
+
+
+
+
+# ------ Testing ------
+# print("Orders cleaned:")
+# print(len(finalOrders))
+# print(finalOrders["postal"].unique())
+# print(finalOrders.describe().to_string())
+# print(finalOrders[["postal"]].describe().to_string())
 
 # eventDF = pd.DataFrame(mapped_events["events"])
 #
+# for event in mapped_events["events"]:
+#     print(event["name"])
+
 # print(mapped_events["events"][0]["name"])
+# print(len(finalEvents))
 
 
